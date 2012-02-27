bb161d8d66ea61889869ba08824d6f5a3254ccbf
Jakub Wilk <jwilk@jwilk.net>
2012-02-27 18:47:42 +0100

    tests: more testing.

--- tests.py
+++ tests.py
@@ -35,7 +35,7 @@ def random_string(size):
         for i in xrange(0, size)
     )
 
-random_blob = random_string(1 << 14)
+random_blob = random_string(1 << 17)
 
 
 def run_hungrycat(options, input_):
@@ -48,7 +48,7 @@ def run_hungrycat(options, input_):
         stderr=ipc.PIPE,
     )
     output = child.stdout.read()
-    errors = child.stderr.read()
+    errors = child.stderr.read().splitlines()
     rc = child.wait()
     if rc == 0:
         assert_false(os.path.exists(input_file))
@@ -57,23 +57,81 @@ def run_hungrycat(options, input_):
             os.unlink(input_file)
     return output, errors, rc
 
-def _test_ftruncate(size, block_size):
+def _standard_test_ftruncate(size, block_size):
     input_ = random_blob[:size]
     output, errors, rc = run_hungrycat(['-s', block_size], input_)
-    assert_equal(output, input_)
-    assert_equal(errors, '')
+    assert_equal(len(output), len(input_))
+    assert_equal(errors, [])
     assert_equal(rc, 0)
+    assert_equal(output, input_)
 
-def test_ftrunacate():
+def _errors_buffer_to_small(errors):
+    try:
+        [message] = errors
+    except ValueError:
+        return
+    return message.endswith(': buffer size too small for fallocate(); falling back to ftruncate()')
+
+def _errors_operation_not_supported(errors, fallback):
+    try:
+        message_notsupp, message_fallback = errors
+    except ValueError:
+        return
+    return (
+        message_notsupp.endswith(': Operation not supported') and
+        message_fallback.endswith('fallocate() failed' + ('; falling back to ftruncate()' if fallback else ''))
+    )
+
+def _standard_test_fallocate(size, block_size):
+    input_ = random_blob[:size]
+    output, errors, rc = run_hungrycat(['-s', block_size, '-P'], input_)
+    if _errors_buffer_to_small(errors):
+        pass
+    elif _errors_operation_not_supported(errors, fallback=True):
+        pass
+    else:
+        assert_equal(errors, [])
+    assert_equal(rc, 0)
+    assert_equal(output, input_)
+
+def _standard_test_force_fallocate(size, block_size):
+    input_ = random_blob[:size]
+    output, errors, rc = run_hungrycat(['-s', block_size, '-P', '-P'], input_)
+    if _errors_buffer_to_small(errors):
+        raise nose.SkipTest
+    elif _errors_operation_not_supported(errors, fallback=False):
+        raise nose.SkipTest
+    else:
+        assert_equal(errors, [])
+    assert_equal(rc, 0)
+    assert_equal(output, input_)
+
+def _standard_test(testfn, min_block_size=1):
     for block_size in xrange(1, 5):
+        if block_size < min_block_size:
+            continue
         for size in xrange(0, block_size * 7 + 3):
-            yield _test_ftruncate, size, block_size
+            yield testfn, size, block_size
     for block_size in 5, 10, 100, 1000, 10000:
+        if block_size < min_block_size:
+            continue
         for n in xrange(0, 8):
             for delta in -2, -1, 0, 1, 2:
                 size = n * block_size + delta
                 if size >= 0:
-                    yield _test_ftruncate, size, block_size
+                    yield testfn, size, block_size
+
+def test_standard_ftruncate():
+    for item in _standard_test(_standard_test_ftruncate):
+        yield item
+
+def test_standard_fallocate():
+    for item in _standard_test(_standard_test_fallocate):
+        yield item
+
+def test_standard_force_fallocate():
+    for item in _standard_test(_standard_test_force_fallocate, min_block_size=8192):
+        yield item
 
 if __name__ == '__main__':
     nose.main()
