1073b26d4ca313430ebf5860d9aa5c083d889885
Jakub Wilk <jwilk@jwilk.net>
2015-06-24 10:34:28 +0200

    Collapse copyright year ranges.

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # encoding=UTF-8
 
-# Copyright © 2012, 2013 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2012-2013 Jakub Wilk <jwilk@jwilk.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the “Software”), to deal
